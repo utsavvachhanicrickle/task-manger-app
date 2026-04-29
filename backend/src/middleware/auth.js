39156739094fbd23f8636107a08ea80fede7d482
utsavvachhanicrickle <utsavvachhani.cs@gmail.com>
2026-04-29 12:13:48 +0530

    11.0 - basic backend changes and the respondes of llm

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -7,7 +7,7 @@ export const authMiddleware = (req, res, next) => {
   if (!token) return res.status(401).json({ message: MESSAGES.ACCESS_DENIED});
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.userId = decoded?.id;
     
     if (!req.userId) {
--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,6 +1,10 @@
 import AIChatModel from "../modules/AiChat.modules.js";
 import { AuthValidation } from "../validations/AuthValidtion.js";
 import { LLMService } from "./llm.service.js";
+import pLimit from "p-limit";
+
+// limit concurrency (VERY IMPORTANT)
+const limit = pLimit(1);
 
 export const aiServices = {
   chatServices: async (data, userId) => {
@@ -14,22 +18,28 @@ export const aiServices = {
 
     const history = await AIChatModel.find({ userId })
       .sort({ createdAt: -1 })
-      .limit(5);
+      .limit(3); 
 
-    const reply = await LLMService.generateResponse(message, history);
+    const reply = await limit(() =>
+      LLMService.generateResponse(message, history)
+    );
 
     const chat = await AIChatModel.create({
       query: message,
       response: reply,
       userId,
     });
+
     return chat;
   },
+
   getHistoryServices: async (userId) => {
     AuthValidation.userExists(userId);
-    const chat = await AIChatModel.find({ userId: userId }).sort({
-      createdAt: 1,
-    });
+
+    const chat = await AIChatModel.find({ userId })
+      .sort({ createdAt: 1 })
+      .limit(100); 
+
     return chat;
   },
 };
